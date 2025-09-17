f1e8f3d30902c38993e9c402eaf17ac64c75a58f
Yiwen Chen <yiwenchen@Yiwens-MacBook-Air.local>
2025-09-16 22:11:37 -0400

    batchified rendering script, priortize env map lighting

--- render_3dmodels_dense_100_150.sh
+++ render_3dmodels_dense_100_150.sh
@@ -5,7 +5,6 @@
 #SBATCH --job-name=render_objaverse_100_150
 #SBATCH --mem=32
 #SBATCH --ntasks=8
-#SBATCH --gpus=1
 #SBATCH --gres=gpu:a5000:1
 #SBATCH --output=myjob.render_objaverse_100_150.out
 #SBATCH --error=myjob.render_objaverse_100_150.err
--- submit_all_groups_gpu.sh
+++ submit_all_groups_gpu.sh
@@ -4,17 +4,6 @@
 echo "开始提交所有渲染作业..."
 
 # 提交各个组的作业
-echo "提交组 200-250..."
-sbatch render_3dmodels_dense_200_250.sh
-
-echo "提交组 250-300..."
-sbatch render_3dmodels_dense_250_300.sh
-
-echo "提交组 300-350..."
-sbatch render_3dmodels_dense_300_350.sh
-
-echo "提交组 350-400..."
-sbatch render_3dmodels_dense_350_400.sh
 
 echo "提交组 400-450..."
 sbatch render_3dmodels_dense_400_450.sh
