88d0fd2ed763751f0bace2b8b8b1b96e606e8a1c
Yiwen Chen <yiwenchen@Yiwens-MacBook-Air.local>
2026-01-23 22:51:34 -0500

    submit jobs on gpu and short

--- scripts/submit_all_groups_8h.sh
+++ scripts/submit_all_groups_8h.sh
@@ -5,16 +5,16 @@ echo "开始提交所有渲染作业..."
 
 # 提交各个组的作业
 echo "提交组 0-50..."
-sbatch render_3dmodels_dense_500-550.sh
+sbatch scripts/render_3dmodels_dense_500-550.sh
 
 echo "提交组 50-100..."
-sbatch render_3dmodels_dense_550-600.sh
+sbatch scripts/render_3dmodels_dense_550-600.sh
 
 echo "提交组 100-150..."
-sbatch render_3dmodels_dense_600-650.sh
+sbatch scripts/render_3dmodels_dense_600-650.sh
 
 echo "提交组 150-200..."
-sbatch render_3dmodels_dense_650-700.sh
+sbatch scripts/render_3dmodels_dense_650-700.sh
 
 echo "所有作业已提交完成!"
 echo "使用 'squeue -u \$USER' 查看作业状态"
--- scripts/submit_all_groups_short.sh
+++ scripts/submit_all_groups_short.sh
@@ -5,15 +5,15 @@ echo "开始提交所有渲染作业..."
 
 # 提交各个组的作业
 echo "提交组 200-250..."
-sbatch render_3dmodels_dense_700-750.sh
+sbatch scripts/render_3dmodels_dense_700-750.sh
 
 echo "提交组 250-300..."
-sbatch render_3dmodels_dense_750-800.sh
+sbatch scripts/render_3dmodels_dense_750-800.sh
 
 echo "提交组 300-350..."
-sbatch render_3dmodels_dense_800-850.sh
+sbatch scripts/render_3dmodels_dense_800-850.sh
 
 echo "提交组 350-400..."
-sbatch render_3dmodels_dense_850-900.sh
+sbatch scripts/render_3dmodels_dense_850-900.sh
 echo "所有作业已提交完成!"
 echo "使用 'squeue -u \$USER' 查看作业状态"
