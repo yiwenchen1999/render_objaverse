54f72b0fcd915d2d7a9481a734a4e84e425244d2
Yiwen Chen <yiwenchen@Yiwens-MacBook-Air.local>
2026-01-26 14:27:28 -0500

    trimming the lighting env to get object variety first

--- scripts/submit_all_groups_short.sh
+++ scripts/submit_all_groups_short.sh
@@ -15,5 +15,12 @@ sbatch scripts/render_3dmodels_dense_800-850.sh
 
 echo "提交组 350-400..."
 sbatch scripts/render_3dmodels_dense_850-900.sh
+
+echo "提交组 900-950..."
+sbatch scripts/render_3dmodels_dense_900-950.sh
+
+echo "提交组 950-1000..."
+sbatch scripts/render_3dmodels_dense_950-1000.sh
+    
 echo "所有作业已提交完成!"
 echo "使用 'squeue -u \$USER' 查看作业状态"
