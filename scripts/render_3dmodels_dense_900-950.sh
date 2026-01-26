8f8c36adda3346e86e8ffd54b9b39d967dfabd59
Yiwen Chen <yiwenchen@Yiwens-MacBook-Air.local>
2026-01-26 14:26:59 -0500

    trimming the lighting env to get object variety first

---
+++ scripts/render_3dmodels_dense_900-950.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --partition=short
+#SBATCH --nodes=1
+#SBATCH --time=24:00:00
+#SBATCH --job-name=render_objaverse_900-950
+#SBATCH --mem=32
+#SBATCH --ntasks=8
+#SBATCH --output=myjob.render_objaverse_900-950.out
+#SBATCH --error=myjob.render_objaverse_900-950.err
+
+# python ../download.py --base_path /projects/vig/Datasets --begin_uid 850 --end_uid 900
+python render_3dmodels_dense.py --group_start 1900 --group_end 1950 \
+--num_views 30 --num_env_lights 3 --num_test_views 50 \
+--num_white_pls 0 --num_rgb_pls 0 --num_multi_pls 0 \
+--num_area_lights 0 --rendered_dir_name rendered_dense_lightPlus
+
+
