b6fbdbba20be883aac768d75caa7fb67aef8ea89
Yiwen Chen <yiwenchen@Yiwens-MacBook-Air.local>
2026-02-02 13:43:21 -0500

    add lines and downloads

--- download_objaverse.sh
+++ download_objaverse.sh
@@ -8,4 +8,4 @@
 #SBATCH --output=myjob.download_objaverse.out
 #SBATCH --error=myjob.download_objaverse.err
 
-python download.py --obj_list test_obj.csv
+python download.py --obj_list test_obj.csv --end_uid 5000
