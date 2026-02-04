b5c11a220d3f33080ef3b822445d2bce4d1aaa92
Yiwen Chen <yiwenchen@Yiwens-MacBook-Air.local>
2026-02-04 00:20:57 -0500

    handling group 5000-6000

--- scripts/render_3dmodels_dense_100_150.sh
+++ scripts/render_3dmodels_dense_100_150.sh
@@ -2,15 +2,15 @@
 #SBATCH --partition=jiang
 #SBATCH --nodes=1
 #SBATCH --time=60:00:00
-#SBATCH --job-name=render_objaverse_4100_4150
+#SBATCH --job-name=render_objaverse_5100_5150
 #SBATCH --mem=32
 #SBATCH --ntasks=8
 #SBATCH --gres=gpu:a5000:1
-#SBATCH --output=myjob.render_objaverse_4100_4150.out
-#SBATCH --error=myjob.render_objaverse_4100_4150.err
+#SBATCH --output=myjob.render_objaverse_5100_5150.out
+#SBATCH --error=myjob.render_objaverse_5100_5150.err
 
 # python ../download.py --base_path /projects/vig/Datasets --begin_uid 100 --end_uid 150
-python render_3dmodels_dense.py --group_start 4100 --group_end 4150 \
+python render_3dmodels_dense.py --group_start 5100 --group_end 5150 \
 --num_views 30 --num_env_lights 3 --num_test_views 50 \
 --num_white_pls 0 --num_rgb_pls 0 --num_multi_pls 0 \
 --num_area_lights 0 --rendered_dir_name rendered_dense_lightPlus
--- scripts/render_3dmodels_dense_350_400.sh
+++ scripts/render_3dmodels_dense_350_400.sh
@@ -2,15 +2,15 @@
 #SBATCH --partition=jiang
 #SBATCH --nodes=1
 #SBATCH --time=60:00:00
-#SBATCH --job-name=render_objaverse_4350_4400
+#SBATCH --job-name=render_objaverse_5350_5400
 #SBATCH --mem=32
 #SBATCH --ntasks=8
 #SBATCH --gres=gpu:a5000:1
-#SBATCH --output=myjob.render_objaverse_4350_4400.out
-#SBATCH --error=myjob.render_objaverse_4350_4400.err
+#SBATCH --output=myjob.render_objaverse_5350_5400.out
+#SBATCH --error=myjob.render_objaverse_5350_5400.err
 
 # python ../download.py --base_path /projects/vig/Datasets --begin_uid 350 --end_uid 400
-python render_3dmodels_dense.py --group_start 4350 --group_end 4400 \
+python render_3dmodels_dense.py --group_start 5350 --group_end 5400 \
 --num_views 30 --num_env_lights 3 --num_test_views 50 \
 --num_white_pls 0 --num_rgb_pls 0 --num_multi_pls 0 \
 --num_area_lights 0 --rendered_dir_name rendered_dense_lightPlus
