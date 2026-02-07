ec31c164126f97b451ed1ebc14b5c1a7a222a6ee
Yiwen Chen <yiwenchen@Yiwens-MacBook-Air.local>
2026-02-06 22:54:28 -0500

    preparing job til 7100

--- scripts/render_3dmodels_dense_900-950.sh
+++ scripts/render_3dmodels_dense_900-950.sh
@@ -2,15 +2,15 @@
 #SBATCH --partition=jiang
 #SBATCH --nodes=1
 #SBATCH --time=60:00:00
-#SBATCH --job-name=render_objaverse_5500_5550
+#SBATCH --job-name=render_objaverse_7150_7200
 #SBATCH --mem=32
 #SBATCH --ntasks=8
 #SBATCH --gres=gpu:a5000:1
-#SBATCH --output=myjob.render_objaverse_5500_5550.out
-#SBATCH --error=myjob.render_objaverse_5500_5550.err
+#SBATCH --output=myjob.render_objaverse_7150_7200.out
+#SBATCH --error=myjob.render_objaverse_7150_7200.err
 
 # python ../download.py --base_path /projects/vig/Datasets --begin_uid 400 --end_uid 450
-python render_3dmodels_dense.py --group_start 5500 --group_end 5550 \
+python render_3dmodels_dense.py --group_start 7150 --group_end 7200 \
 --num_views 30 --num_env_lights 3 --num_test_views 50 \
 --num_white_pls 0 --num_rgb_pls 0 --num_multi_pls 0 \
 --num_area_lights 0 --rendered_dir_name rendered_dense_lightPlus
