ce26bbe53ca52241ead47bdc56ff317d3fe936a0
Yiwen Chen <yiwenchen@Yiwens-MacBook-Air.local>
2026-02-10 11:30:26 -0500

    rendering job updated to group 8700

--- scripts/render_3dmodels_dense_650-700.sh
+++ scripts/render_3dmodels_dense_650-700.sh
@@ -2,15 +2,15 @@
 #SBATCH --partition=gpu
 #SBATCH --nodes=1
 #SBATCH --time=8:00:00
-#SBATCH --job-name=render_objaverse_6350-6400
+#SBATCH --job-name=render_objaverse_8550-8600
 #SBATCH --mem=32
 #SBATCH --ntasks=8
 #SBATCH --gres=gpu:1
-#SBATCH --output=myjob.render_objaverse_6350-6400.out
-#SBATCH --error=myjob.render_objaverse_6350-6400.err
+#SBATCH --output=myjob.render_objaverse_8550-8600.out
+#SBATCH --error=myjob.render_objaverse_8550-8600.err
 
 # python ../download.py --base_path /projects/vig/Datasets --begin_uid 650 --end_uid 700
-python render_3dmodels_dense.py --group_start 6350 --group_end 6400 \
+python render_3dmodels_dense.py --group_start 8550 --group_end 8600 \
 --num_views 30 --num_env_lights 3 --num_test_views 50 \
 --num_white_pls 0 --num_rgb_pls 0 --num_multi_pls 0 \
 --num_area_lights 0 --rendered_dir_name rendered_dense_lightPlus
--- scripts/render_3dmodels_dense_700-750.sh
+++ scripts/render_3dmodels_dense_700-750.sh
@@ -2,15 +2,15 @@
 #SBATCH --partition=gpu
 #SBATCH --nodes=1
 #SBATCH --time=8:00:00
-#SBATCH --job-name=render_objaverse_6400-6450
+#SBATCH --job-name=render_objaverse_8650-8700
 #SBATCH --mem=32
 #SBATCH --ntasks=8
 #SBATCH --gres=gpu:1
-#SBATCH --output=myjob.render_objaverse_6400-6450.out
-#SBATCH --error=myjob.render_objaverse_6400-6450.err
+#SBATCH --output=myjob.render_objaverse_8650-8700.out
+#SBATCH --error=myjob.render_objaverse_8650-8700.err
 
 # python ../download.py --base_path /projects/vig/Datasets --begin_uid 550 --end_uid 600
-python render_3dmodels_dense.py --group_start 6400 --group_end 6450 \
+python render_3dmodels_dense.py --group_start 8650 --group_end 8700 \
 --num_views 30 --num_env_lights 3 --num_test_views 50 \
 --num_white_pls 0 --num_rgb_pls 0 --num_multi_pls 0 \
 --num_area_lights 0 --rendered_dir_name rendered_dense_lightPlus
