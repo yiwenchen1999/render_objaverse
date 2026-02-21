4a3e444da54189da0d394545d65ffe2def3364ed
Yiwen Chen <yiwenchen@Yiwens-MacBook-Air.local>
2026-02-21 15:13:02 -0500

    attach texture test run

--- setup.sh
+++ setup.sh
@@ -10,10 +10,9 @@ source venv/bin/activate
 
 srun --partition=gpu --nodes=1 --pty --gres=gpu:1 --ntasks=8 --mem=32 --time=8:00:00 /bin/bash
 
-python render_3d_scenes_dense.py \
+python render_3dmodels_dense_addLights.py \
     --group_start 0 \
-    --group_end 10 \
+    --group_end 1 \
     --num_white_envs 1 \
-    --num_env_lights 5 \
-    --texture_dir /projects/vig/Datasets/Polyhaven/polyhaven_textures \
-    --output_dir ./output_scenes
+    --num_env_lights 0 \
+    --output_dir ./output_scenes_dense
