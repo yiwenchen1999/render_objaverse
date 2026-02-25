6cfc46f4116cd59e66fa95574e8d94184a085699
Yiwen Chen <yiwenchen@Yiwens-MacBook-Air.local>
2026-02-25 00:25:37 -0500

    tuning cam placement

--- render_3dscenes_dense.py
+++ render_3dscenes_dense.py
@@ -520,8 +520,8 @@ def render_core(args: Options, groups_id = 0):
         eyes = gen_random_pts_around_origin(
             seed=seed_view,
             N=args.num_views,                # set to a large value (e.g. 100, 200, 400)
-            min_dist_to_origin=3.0,
-            max_dist_to_origin=3.0,          # usually keep min=max for consistent radius
+            min_dist_to_origin=2.5,
+            max_dist_to_origin=2.5,          # usually keep min=max for consistent radius
             min_theta_in_degree=20,           # 0 for full sphere, 10/20 for hemisphere
             max_theta_in_degree=80,         # 90 or 70 for upper hemisphere only
             z_up=True
@@ -529,8 +529,8 @@ def render_core(args: Options, groups_id = 0):
         eyes_traj = gen_pt_traj_around_origin(
             seed=seed_view,
             N=args.num_test_views,
-            min_dist_to_origin=3,
-            max_dist_to_origin=3,
+            min_dist_to_origin=2.5,
+            max_dist_to_origin=2.5,
             theta_in_degree=60,
             z_up=True
         )
