2b52159a2134e4f2ec2a7b0c8e3e53b18de887c4
Yiwen Chen <yiwenchen@Yiwens-MacBook-Air.local>
2026-02-26 16:10:09 -0500

    distributed rendering 0-60

--- SonyAIClusterUtil/run_render_3dmodels_dense_polyhaven_distributed_0_60.sh
+++ SonyAIClusterUtil/run_render_3dmodels_dense_polyhaven_distributed_0_60.sh
@@ -25,7 +25,7 @@ cd "$PROJ"
 # Multi-worker: 4 Blender processes per GPU
 python3 SonyAIClusterUtil/distribute_render_polyhaven.py \
   --num_gpus 1 \
-  --workers_per_gpu 4 \
+  --workers_per_gpu 8 \
   --group_start 0 \
   --group_end 60 \
   --model_list_path assets/object_ids/polyhaven_models_train.json \
