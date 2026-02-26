42b6d073376ef4aebd5de2509c9c99d569de38d7
Yiwen Chen <yiwenchen@Yiwens-MacBook-Air.local>
2026-02-26 17:41:46 -0500

    cannot do distributed training on a6000

--- scripts/distribute_render_3dscenes_0_50.sh
+++ scripts/distribute_render_3dscenes_0_50.sh
@@ -19,8 +19,8 @@ cd /projects/vig/yiwenc/ResearchProjects/lightingDiffusion/3dgs/render_objaverse
 python scripts/distribute_render_3dscenes.py \
   --num_gpus 1 \
   --workers_per_gpu 4 \
-  --group_start 0 \
-  --group_end 50 \
+  --group_start 5 \
+  --group_end 10 \
   --num_white_envs 1 \
   --num_env_lights 3 \
   --num_white_pls 0 --num_rgb_pls 0 --num_multi_pls 0 \
