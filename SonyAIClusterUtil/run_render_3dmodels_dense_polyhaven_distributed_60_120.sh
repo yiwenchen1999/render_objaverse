238050c8734ba67b010d1c91a6bbfb88683c0f2c
Yiwen Chen <yiwenchen@Yiwens-MacBook-Air.local>
2026-02-27 20:07:46 -0500

    render polyhaven assets_test

--- SonyAIClusterUtil/run_render_3dmodels_dense_polyhaven_distributed_60_120.sh
+++ SonyAIClusterUtil/run_render_3dmodels_dense_polyhaven_distributed_60_120.sh
@@ -29,7 +29,7 @@ python3 SonyAIClusterUtil/distribute_render_polyhaven.py \
   --group_start 0 \
   --group_end 60 \
   --model_list_path assets/object_ids/polyhaven_models_test.json \
-  --output_dir /music-shared-disk/group/ct/yiwen/data/objaverse/rendered_dense_polyhaven \
+  --output_dir /music-shared-disk/group/ct/yiwen/data/objaverse/rendered_dense_polyhaven_test \
   --model_lq_dir /music-shared-disk/group/ct/yiwen/data/objaverse/polyhaven_models \
   --env_map_dir /music-shared-disk/group/ct/yiwen/data/objaverse/hdris \
   --num_views 30 \
