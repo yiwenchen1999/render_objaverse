99a5a6b94ee44da0efde69bbe0476d3dd982f6bb
Yiwen Chen <yiwenchen@Yiwens-MacBook-Air.local>
2026-04-28 20:35:16 -0400

    download objs

---
+++ scripts/download_objaverse.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH --partition=jiang
+#SBATCH --nodes=1
+#SBATCH --time=48:00:00
+#SBATCH --job-name=download_objaverse
+#SBATCH --mem=32
+#SBATCH --ntasks=8
+#SBATCH --output=myjob.download_objaverse.out
+#SBATCH --error=myjob.download_objaverse.err
+
+python download.py --obj_list test_obj.csv --end_uid 20000 --base_path /music-shared-disk/group/ct/yiwen/data/objaverse
