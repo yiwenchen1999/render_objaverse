3696d8e7ca12b679b435789aa5952a087ed9b90e
Yiwen Chen <yiwenchen@Yiwens-MacBook-Air.local>
2026-05-06 01:57:31 -0400

    dynamic scale

--- scripts/render_objs_enhanced/render_objs_enhanced.sh
+++ scripts/render_objs_enhanced/render_objs_enhanced.sh
@@ -19,8 +19,8 @@ cd /projects/vig/yiwenc/ResearchProjects/lightingDiffusion/3dgs/render_objaverse
 python scripts/distribute_render_3dmodels_dense_enhance.py \
   --num_gpus 1 \
   --workers_per_gpu 2 \
-  --group_start 16000 \
-  --group_end 16050 \
+  --group_start 17000 \
+  --group_end 17050 \
   --num_views 30 \
   --num_test_views 50 \
   --dynamic_lighting_counts \
